aefff87bc594ece857d5eaf00133d30eb860ab4a
baptiste dauphin <bap_dauphin@hotmail.fr>
2021-05-17 18:28:42 +0200

    finished routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :pigs do
-    resources :bookings, :only [ :new, :create ]
+    resources :bookings, only: [:new, :create]
   end
+
   get "dashboard", to: "bookings#dashboard"
-  get "dashboard/bookings/:id", to: "bookings#edit"
-  patch "dashboard/bookings/:id", to: "bookings#accept"
+  patch "dashboard", to: "bookings#accept"
+  patch "dashboard", to: "bookings#decline"
 end
