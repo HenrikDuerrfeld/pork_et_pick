9e431a2cf62e451b45ca5158b04eb65176ef7818
baptiste dauphin <bap_dauphin@hotmail.fr>
2021-05-17 18:12:22 +0200

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :pigs do
+    resources :bookings, :only [ :new, :create ]
+  end
+  get "dashboard", to: "bookings#dashboard"
+  get "dashboard/bookings/:id", to: "bookings#edit"
+  patch "dashboard/bookings/:id", to: "bookings#accept"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_17_145714) do
+ActiveRecord::Schema.define(version: 2021_05_17_155218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_05_17_145714) do
     t.integer "total"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "status"
     t.index ["pig_id"], name: "index_bookings_on_pig_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
