50806d7ac52ae802049f75a0cc186168a6359319
baptiste dauphin <bap_dauphin@hotmail.fr>
2021-05-17 17:09:54 +0200

    completed models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_17_144120) do
+ActiveRecord::Schema.define(version: 2021_05_17_145714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "pig_id", null: false
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "total"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["pig_id"], name: "index_bookings_on_pig_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "pigs", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "price_per_day"
+    t.boolean "status"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_pigs_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +53,7 @@ ActiveRecord::Schema.define(version: 2021_05_17_144120) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "pigs"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "pigs", "users"
 end
