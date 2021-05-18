c474b92a2b3d1624ce353293a7143e8b1a2d2844
Mamoun Benbrahim <mamounbenbra@gmail.com>
2021-05-18 12:19:45 +0200

    Pigs controller and views

--- app/controllers/pigs_controller.rb
+++ app/controllers/pigs_controller.rb
@@ -1,2 +1,48 @@
 class PigsController < ApplicationController
+
+  def index
+    @pigs = Pig.all
+  end
+
+  def show
+    @pig = Pig.find(params[:id])
+  end
+
+  def new
+    @pig = Pig.new
+  end
+
+  def create
+    @pig = Pig.new(pig_params)
+    @pig.user = current_user
+    if @pig.save
+      redirect_to pig_path(@pig)
+    else
+      raise
+      render :new
+    end
+  end
+
+  def edit
+    @pig = Pig.find(params[:id])
+  end
+
+  def update
+    @pig = Pig.find(params[:id])
+    @pig.update(pig_params)
+    redirect_to pig_path(@pig)
+  end
+
+  def destroy
+    @pig = Pig.find(params[:id])
+    @pig.destroy
+    redirect_to pigs_path
+  end
+
+  private
+
+  def pig_params
+    params.require(:pig).permit(:description, :name, :price_per_day, :status, :user_id)
+  end
+
 end
--- app/models/pig.rb
+++ app/models/pig.rb
@@ -2,6 +2,6 @@ class Pig < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   validates :name, presence: true
-  validates :price, presence: true
+  validates :price_per_day, presence: true
   validates :description, presence: true
 end
