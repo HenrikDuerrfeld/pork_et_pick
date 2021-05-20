1b1db00e7e0df75ebca7aa48dcc8468663225c42
Dumont Julien <lapz95@hotmail.fr>
2021-05-20 15:44:52 +0200

    merge

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -31,3 +31,7 @@ import { initMapbox } from '../plugins/init_mapbox';
 document.addEventListener('turbolinks:load', () => {
   initMapbox();
 })
+
+import { initFlatpickr } from "../plugins/flatpickr";
+
+initFlatpickr();
