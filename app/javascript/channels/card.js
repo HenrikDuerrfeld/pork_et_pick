fdd32597150674f3451a51dae5dd967a06897ff1
Mamoun Benbrahim <mamounbenbra@gmail.com>
2021-05-21 15:16:58 +0200

    card

---
+++ app/javascript/channels/card.js
@@ -0,0 +1,7 @@
+const cardHover = document.querySelector('#oui').forEach ((card) => {
+    card.addEventListener("click", (event) => {
+    console.log(event.currentTarget)
+    )};
+  });
+
+export { cardHover };
